b5607cfa5aabd8006a951ad9e34b6307dc418f67
infalliblePirate <pavlenko.kateryna@lll.kpi.ua>
2025-07-08 21:44:27 +0300

    Add bnb testnet configuration to hardhat.

--- packages/onchain/hardhat.config.js
+++ packages/onchain/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    bnb: { // test
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      accounts: {
+        mnemonic: process.env.MNEMONIC
+      }
+    }
+  }
 };
