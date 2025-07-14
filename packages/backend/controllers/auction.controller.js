2d57862d56043a6ae1a9602af09dc9fb13b16cdd
infalliblePirate <pavlenko.kateryna@lll.kpi.ua>
2025-07-14 13:46:13 +0300

    Refactor auction creation to use AuctionFactory contract.

--- packages/backend/controllers/auction.controller.js
+++ packages/backend/controllers/auction.controller.js
@@ -2,7 +2,7 @@ const auctionService = require('../services/auction.service')
 
 exports.createAuction = async (req, res) => {
     try {
-        const deployedAddr = await auctionService.deployAuction(req.body);
+        const deployedAddr = await auctionService.createAuction(req.body);
         res.status(201).json({ "address": deployedAddr });
     } catch (error) {
         console.error('Auction deployment failed:', error);
--- packages/backend/services/auction.service.js
+++ packages/backend/services/auction.service.js
@@ -4,22 +4,48 @@ const path = require('path');
 // the working directory to the onchain/ folder
 // so that Hardhat can initialize properly
 process.chdir(path.join(__dirname, '../../onchain'));
+process.env.HARDHAT_NETWORK = 'bnb';
 
 const hre = require('hardhat');
-const AuctionModule = require('../../onchain/ignition/modules/Auction');
 
-exports.deployAuction = async ({ duration }) => {
+exports.createAuction = async ({ biddingTime }) => {
+  try {
+    const [deployer] = await hre.ethers.getSigners();
+    const beneficiary = deployer.address;
 
-    const { auction } = await hre.ignition.deploy(AuctionModule, {
-        parameters: {
-            AuctionModule: {
-                duration
-            }
-        }
-    })
+    const factoryAddress = "0xCdE8F1B0933f4b9EeF475b8C0A9FCda0c5C160e2";
+    const factory = await hre.ethers.getContractAt("AuctionFactory", factoryAddress);
 
-    const address = await auction.getAddress();
-    console.log(`Auction deployed to: ${address}`)
+    const tx = await factory.createAuction(biddingTime, beneficiary);
+    const receipt = await tx.wait();
 
-    return address;
-}
+    if (receipt.status !== 1) {
+      throw new Error('Transaction failed');
+    }
+
+    const auctionCreatedEvents = receipt.logs.filter(log => {
+      return log.address.toLowerCase() === factoryAddress.toLowerCase() &&
+        log.topics[0] === factory.interface.getEvent('AuctionCreated').topicHash;
+    });
+
+    const auctionCreatedEvent = factory.interface.parseLog(auctionCreatedEvents[0]);
+
+    if (!auctionCreatedEvent) {
+      throw new Error('AuctionCreated event not found in transaction receipt');
+    }
+
+    const [auctionAddress, eventBeneficiary, eventBiddingTime] = auctionCreatedEvent.args;
+
+    console.log(`Auction created successfully at address: ${auctionAddress}`);
+
+    return {
+      auctionAddress: auctionAddress,
+      beneficiary: eventBeneficiary,
+      biddingTime: Number(eventBiddingTime)
+    };
+  } catch (error) {
+    console.error("Auction deployment failed:", error);
+    throw error;
+  }
+};
+
